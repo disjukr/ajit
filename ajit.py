3f995cd3ee354418dbf96edc972ae3f0876bc18d
JongChan Choi <disjukr@naver.com>
2014-08-09 01:23:20 +0900

    parse

--- ajit.py
+++ ajit.py
@@ -1,6 +1,42 @@
 import os
 import sys
 
+def extract_index(func):
+    def extractor(code):
+        return func(ord(code) - 0xac00)
+    return extractor
+
+@extract_index
+def get_cho(syllable_code):
+    return syllable_code // 588
+
+@extract_index
+def get_jung(syllable_code):
+    return (syllable_code // 28) % 21
+
+@extract_index
+def get_jong(syllable_code):
+    return syllable_code % 28
+
+def parse(code):
+    result = []
+    line = []
+    for char in code:
+        if char == "\r":
+            continue
+        elif char == "\n":
+            result.append(line)
+            line = []
+        else:
+            line.append(char)
+    result.append(line)
+    return result
+
+def run(code):
+    codespace = parse(code)
+    code = codespace[0][0]
+    print get_cho(code), get_jung(code), get_jong(code)
+
 def entry_point(argv):
     # filename = argv[0]
     filename = "99dan.aheui"
@@ -12,7 +48,7 @@ def entry_point(argv):
             break
         code += read
     code = code.decode("utf-8")
-    print code.encode("utf-8")
+    run(code)
     return 0
 
 def target(*args):
