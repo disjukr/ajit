2b74eabe857c494f17cad89642ca05ea2c8d968d
JongChan Choi <disjukr@naver.com>
2014-08-12 14:01:49 +0900

    cursor

--- ajit.py
+++ ajit.py
@@ -1,9 +1,48 @@
 import os
 import sys
 
+class Cursor:
+    def __init__(self, codespace, x=0, y=0, dx=0, dy=1):
+        self.codespace = codespace
+        self.x = x
+        self.y = y
+        self.dx = dx
+        self.dy = dy
+
+    def line(self):
+        try:
+            line = self.codespace[self.y]
+        except IndexError:
+            line = None
+        return line
+
+    def code(self):
+        return get_code(self.codespace, self.x, self.y)
+
+    def move(self):
+        self.x = self.x + self.dx
+        self.y = self.y + self.dy
+        line = self.line()
+        width = len(line) if line is not None else 0
+        height = len(self.codespace)
+        if self.x < 0 and self.dx < 0:
+            self.x = width - 1
+        elif self.x >= width and self.dx > 0:
+            self.x = 0
+        if self.y < 0 and self.dy < 0:
+            self.y = height - 1
+        elif self.y >= height and self.dy > 0:
+            self.y = 0
+
+class Machine:
+    def __init__(self, codespace):
+        self.cursor = Cursor(codespace)
+
 def extract_index(func):
     def extractor(code):
-        return func(ord(code) - 0xac00)
+        if 0xac00 <= code <= 0xd7a3:
+            return func(code - 0xac00)
+        return -1
     return extractor
 
 @extract_index
@@ -18,24 +57,41 @@ def get_jung(syllable_code):
 def get_jong(syllable_code):
     return syllable_code % 28
 
+def get_code(codespace, x, y):
+    try:
+        line = codespace[y]
+    except IndexError:
+        return (-1, -1, -1)
+    try:
+        code = line[x]
+    except IndexError:
+        return (-1, -1, -1)
+    return code
+
 def parse(code):
     result = []
     line = []
     for char in code:
-        if char == "\r":
+        if char == u"\r":
             continue
-        elif char == "\n":
+        elif char == u"\n":
             result.append(line)
             line = []
         else:
-            line.append(char)
+            charcode = ord(char)
+            line.append(
+                (get_cho(charcode),
+                 get_jung(charcode),
+                 get_jong(charcode)))
     result.append(line)
     return result
 
 def run(code):
-    codespace = parse(code)
-    code = codespace[0][0]
-    print get_cho(code), get_jung(code), get_jong(code)
+    machine = Machine(parse(code))
+    # TODO: run machine
+    machine.cursor.move()
+    code = machine.cursor.code()
+    print code
 
 def entry_point(argv):
     # filename = argv[0]
